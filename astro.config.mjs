8cb0cfa9a29e8b5dd56d4d74cc36297260d5223d
VillaAgaete <emendez@aytoagaete.es>
2024-07-24 09:09:32 +0100

    changes to make it work with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel()
 });
