f241effd82edca54d88acfa1022ff48f1bee36cc
VillaAgaete <emendez@aytoagaete.es>
2024-07-24 11:47:00 +0100

    paths fixed to always /

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import path from 'path';
 
 export default defineConfig({
+  build: {
+    // Default output directory for Astro builds
+    outDir: path.join('dist', 'entry.pages').replace(/\\/g, '/'),
+  },
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
 });
