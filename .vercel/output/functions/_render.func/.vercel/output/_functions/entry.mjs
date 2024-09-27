9492cc96894ab5b894c6d03700eaca5e07223755
VillaAgaete <emendez@aytoagaete.es>
2024-09-27 13:46:34 +0100

    another one

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_SrcqTSjU.mjs';
-import { manifest } from './manifest_lUYFV8un.mjs';
+import { manifest } from './manifest_B5AWQQ2D.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/admin.astro.mjs');
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "5cb8427f-3124-47f1-a73b-c0faaa5f1a86",
+    "middlewareSecret": "2b379d1b-e6ec-404b-90c2-09c39cece251",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
