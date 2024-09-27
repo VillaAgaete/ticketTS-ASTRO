375e9aff56ef95060206612e18834b46131ba197
VillaAgaete <emendez@aytoagaete.es>
2024-09-27 09:55:42 +0100

    again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-   import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
-   export default defineConfig({
-     output: 'server',
-     adapter: vercel(),
-   });
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
+});
